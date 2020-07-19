ce9fe7675b40e5b8393b14166f8e187f5cf82d57
TheShubham-K <subham.kumar03@gmail.com>
2020-07-19 08:38:43 +0530

    Updates

---
+++ precache-manifest.099c023b03e168fac67590d12a956436.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "233a4dda09a2c9837d6681b8ae79f6e6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b6cae6c3afe0fad0a516",
+    "url": "/Portfolio/static/css/main.8134bbd6.chunk.css"
+  },
+  {
+    "revision": "37aeebcf6feab626ea1d",
+    "url": "/Portfolio/static/js/2.0b6c0ac6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.0b6c0ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6cae6c3afe0fad0a516",
+    "url": "/Portfolio/static/js/main.91cdcbc0.chunk.js"
+  },
+  {
+    "revision": "82291101b3ad160f6b60",
+    "url": "/Portfolio/static/js/runtime-main.cf36be62.js"
+  }
+]);
