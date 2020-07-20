26f94713a36b41d12b05657e50a98293ffe490b6
TheShubham-K <subham.kumar03@gmail.com>
2020-07-20 13:00:00 +0530

    Updated

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,43 +1,49 @@
 export const projects = {
     heading: "Projects",
     quote: "Ahh, Learning alone is boring, implementation is the real fun",
-    projectList: [
-        {
-            name: "Project 1",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 2",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 3",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 4",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 5",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 6",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 7",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 8",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }, {
-            name: "Project 9",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            link: "http://madhavbahl.tech/react-pokedex/"
-        }
-    ]
+    projectList: [{
+        name: "Project 1",
+        description: "Introduction-to-TensorFlow-for-Artificial-Intelligence-Machine-Learning-and-Deep-Learning.",
+        link: "https://github.com/TheShubham-K/Introduction-to-TensorFlow-for-Artificial-Intelligence-Machine-Learning-and-Deep-Learning"
+    }, {
+        name: "Project 2",
+        description: "A Complete JavaScript Course build by me.",
+        link: "https://github.com/TheShubham-K/JavaScript"
+    }, {
+        name: "Project 3",
+        description: "This Repository has complete course of python 3 Programming by university of Michigan.",
+        link: "https://github.com/TheShubham-K/Python-Basic-Assessments"
+    }, {
+        name: "Project 4",
+        description: "Algorithm - Toolbox.",
+        link: "https://github.com/TheShubham-K/Algorithmic_Toolbox"
+    }, {
+        name: "Project 5",
+        description: "Predict Employees TurnOver with Scikit-Learn.",
+        link: "https://github.com/TheShubham-K/Predict-Employee-Turnover-With-Scikit-Learn"
+    }, {
+        name: "Project 6",
+        description: "A Community - driven python bot that aims to be as simple as possible to serve humans with their everyday tasks ",
+        link: " http: //m.me/J.A.R.V.I.S.on.Messenger"
+    }, {
+        name: "Project 7",
+        description: "Artic-Monkey Project",
+        link: "https://github.com/TheShubham-K/Artic_Monkey"
+    }, {
+        name: "Project 8",
+        description: "BitCoin-Master Project.",
+        link: "https://theshubham-k.github.io/Bitcoin-Master/"
+    }, {
+        name: "Project 9",
+        description: "Covid-19 Tracker Project.",
+        link: "https://theshubham-k.github.io/Covid-19-Tracker/"
+    }, {
+        name: "Project 10",
+        description: "Pillow-Tessaract and opencv.",
+        link: "https://github.com/TheShubham-K/Python-Project-pillow-tesseract-and-opencv"
+    }, {
+        name: "Project 11",
+        description: "opencv.",
+        link: "https://github.com/TheShubham-K/opencv"
+    }]
 }
